0e8f44cde6e8e6d8c2464a6dd550f51d1d4c72bd
hjbar <hjbar@protonmail.com>
2025-10-23 22:01:55 +0200

    First version of in-lining

--- src/inlining.ml
+++ src/inlining.ml
@@ -16,18 +16,101 @@ let hashtbl_from_nodes nodes =
   ht
 
 
+(* Renvoie les outputs d'un noeud en inlinant les tuples *)
+
+let get_outputs node =
+  let output_ids = List.map fst node.tn_output in
+
+  let rec loop equs acc =
+    match equs with
+    | [] -> List.rev acc
+    | equ :: equs -> begin
+      match equ.teq_patt.tpatt_desc with
+      | [] -> assert false
+      | [ left_id ] ->
+        if List.mem left_id output_ids then loop equs (equ :: acc)
+        else loop equs acc
+      | ids ->
+        let types = equ.teq_patt.tpatt_type in
+        let zip = List.combine ids types in
+
+        let exprs =
+          match equ.teq_expr.texpr_desc with
+          | TE_tuple es -> es
+          | _ -> assert false
+        in
+
+        let new_equs =
+          List.map2
+            (fun (id, ty) e ->
+              let teq_patt =
+                {
+                  tpatt_desc = [ id ];
+                  tpatt_type = [ ty ];
+                  tpatt_loc = equ.teq_patt.tpatt_loc;
+                }
+              in
+              let teq_expr = e in
+              { teq_patt; teq_expr } )
+            zip exprs
+        in
+
+        loop equs (new_equs @ acc)
+    end
+  in
+  loop node.tn_equs []
+
+
+(* Renvoie les outputs en une seule équation avec un tuple
+   Pré-condition: aucun tuple dans les équations du noeud *)
+
+let combine_outputs_equs node output_equs =
+  let output_ids = List.map fst node.tn_output in
+
+  let rec sort output_ids acc =
+    match output_ids with
+    | [] -> List.rev acc
+    | output_id :: output_ids ->
+      let equ =
+        List.find
+          (fun equ -> List.hd equ.teq_patt.tpatt_desc = output_id)
+          output_equs
+      in
+      sort output_ids (equ :: acc)
+  in
+
+  let loc = (List.hd output_equs).teq_patt.tpatt_loc in
+
+  let rec make_tuple output_equs ids tys es =
+    match output_equs with
+    | [] ->
+      let ids = List.rev ids in
+      let tys = List.rev tys in
+      let es = List.rev es in
+
+      let teq_patt = { tpatt_desc = ids; tpatt_type = tys; tpatt_loc = loc } in
+
+      let teq_expr =
+        { texpr_desc = TE_tuple es; texpr_type = tys; texpr_loc = loc }
+      in
+
+      { teq_patt; teq_expr }
+    | output_equ :: output_equs ->
+      let id = output_equ.teq_patt.tpatt_desc |> List.hd in
+      let ty = output_equ.teq_patt.tpatt_type |> List.hd in
+      let e = output_equ.teq_expr in
+
+      make_tuple output_equs (id :: ids) (ty :: tys) (e :: es)
+  in
+
+  let output_equs = sort output_ids [] in
+  make_tuple output_equs [] [] []
+
+
 (* Inline une liste d'équations en une unique équation *)
 
 let inline_in_node ht node =
-  let outputs = List.map fst node.tn_output in
-  let output_eqns =
-    List.filter
-      (fun eqn ->
-        (* TODO: Supporter tuple à gauche*)
-        let left_id = List.hd eqn.teq_patt.tpatt_desc in
-        List.mem left_id outputs )
-      node.tn_equs
-  in
+  let output_eqns = get_outputs node in
 
   let rec replace left_id e =
     let desc =
@@ -36,14 +119,13 @@ let inline_in_node ht node =
       | TE_ident id -> begin
         match
           List.find_opt
-            (* TODO: Supporter tuple à gauche *)
             (fun eqn ->
               let current_id = List.hd eqn.teq_patt.tpatt_desc in
               current_id <> left_id && current_id = id )
             node.tn_equs
         with
         | None -> TE_ident id
-        | Some eqn -> (replace left_id eqn.teq_expr).texpr_desc
+        | Some eqn -> eqn.teq_expr.texpr_desc
       end
       | TE_op (op, es) -> TE_op (op, List.map (replace left_id) es)
       | TE_app (f, args) -> TE_app (f, List.map (replace left_id) args)
@@ -55,14 +137,13 @@ let inline_in_node ht node =
     { e with texpr_desc = desc }
   in
 
-  (* TODO: Supporter tuple à gauche *)
   output_eqns
   |> List.map (fun eqn ->
        {
          eqn with
          teq_expr = replace (List.hd eqn.teq_patt.tpatt_desc) eqn.teq_expr;
        } )
-  |> List.hd
+  |> combine_outputs_equs node
 
 
 (* Remplace les paramètres par les arguments *)
@@ -110,7 +191,6 @@ let rec replace_expr ht left_ids f args =
     { e with texpr_desc = desc }
   in
 
-  (* TODO: supporter plusieurs équations *)
   let eqn = inline_in_node ht node in
   (replace eqn.teq_expr).texpr_desc
 
@@ -128,7 +208,16 @@ and inline_from_expr ht left_ids e =
     | TE_arrow (e1, e2) ->
       TE_arrow (inline_from_expr ht left_ids e1, inline_from_expr ht left_ids e2)
     | TE_pre e -> TE_pre (inline_from_expr ht left_ids e)
-    | TE_tuple es -> TE_tuple (inline_from_expr_list ht left_ids es)
+    | TE_tuple es ->
+      let rec loop es idx acc =
+        match es with
+        | [] -> List.rev acc
+        | e :: es ->
+          let left_id = List.nth left_ids idx in
+          let new_e = inline_from_expr ht [ left_id ] e in
+          loop es (idx + 1) (new_e :: acc)
+      in
+      TE_tuple (loop es 0 [])
   in
   { e with texpr_desc = desc }
 
